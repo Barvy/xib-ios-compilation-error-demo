ebba68232021ea0630dbba9159aecb37d08ce0da
Dmytro Bondarenko <bondarenkod@windowslive.com>
2020-08-13 23:22:30 +0300

    added demo to repro the issue

---
+++ CustomView/CustomViewProxy.cs
@@ -0,0 +1,17 @@
+﻿using CustomViewExample;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomViewExample
+{
+    [Register("CustomViewProxy")]
+    partial class CustomViewProxy : CustomView
+    {
+        public CustomViewProxy(IntPtr handle) : base(handle)
+        {
+        }
+    }
+}
